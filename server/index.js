763fcc5fe3dba43cdcbde3e0cbedee469aeb35b5
_Vel_Murugan_ <140421065+Velmurugan9566@users.noreply.github.com>
2024-06-07 23:23:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const usermodel = require('./module/register')
 const app = express()
 app.use(cors(
     {
-        orgin:['https://simple-login-frontend-seven.vercel.app'],
+        orgin:['https://simple-login-frontend-seven.vercel.app/'],
         method:['POST','GET'],
         credentials:true
     }
