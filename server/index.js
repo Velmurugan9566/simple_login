af602b2f182bc5388fabd0ca40b3fad1af2f6f9a
_Vel_Murugan_ <140421065+Velmurugan9566@users.noreply.github.com>
2024-06-08 20:55:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,73 +1,70 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors')
-const usermodel = require('./module/register')
-const app = express()
-app.use(cors(
-    {
-        orgin:["https://simple-login-frontend-seven.vercel.app"],
-        methods:["POST","GET","PUT"],
-        credentials : true
-    }
-));
-app.use(express.json())
+const cors = require('cors');
+const usermodel = require('./module/register');
+const app = express();
 
-mongoose.connect('mongodb+srv://velmca24:vel9566@cluster0.i4qp0rb.mongodb.net/velu?retryWrites=true&w=majority&appName=Cluster0');
+app.use(cors({
+    origin: ["https://simple-login-frontend-seven.vercel.app"],
+    methods: ["POST", "GET", "PUT"],
+    credentials: true
+}));
 
-app.post('/register', (req,res) => {
-    const {name,age,phone,email,password} =req.body;
-    usermodel.findOne({email:email})
-    .then(user =>{
-        if (user){
-            res.json({status:2})
-        }
-        else{
-            usermodel.create(req.body)
-            .then(user => res.json({status:1}))
-            .catch(err => res.json(err))
-        }
-    })
-    .catch(err => res.json(err))
-    
-})
-app.post('/login', (req,res) => {
-    const {email,password} =req.body;
-    usermodel.findOne({email:email})
-    .then(user =>{
-        if (user){
-            if(user.password != password){
-                res.json({status:3})
-            }else{
-                res.json({status:1})
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://velmca24:vel9566@cluster0.i4qp0rb.mongodb.net/velu?retryWrites=true&w=majority&appName=Cluster0', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+app.post('/register', (req, res) => {
+    const { name, age, phone, email, password } = req.body;
+    usermodel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                res.json({ status: 2 });
+            } else {
+                usermodel.create(req.body)
+                    .then(user => res.json({ status: 1 }))
+                    .catch(err => res.json(err));
             }
-        }
-        else{
-            res.json({status:2})
-        }
-    })
-    .catch(err => res.json(err))
+        })
+        .catch(err => res.json(err));
+});
 
-})
-app.put('/forgetps', (req,res) => {
-    const {email,password} =req.body;
-    usermodel.findOne({email:email})
-    .then(user =>{
-        if (user){
-            usermodel.updateOne({
-                password:password
-            })
-            .then(user => res.json({status:1}))
-            .catch(err => res.json(err))
-        }
-        else{
-           res.json({status:2})
-        }
-    })
-    .catch(err => res.json(err))
-    
-})
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    usermodel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                if (user.password !== password) {
+                    res.json({ status: 3 });
+                } else {
+                    res.json({ status: 1 });
+                }
+            } else {
+                res.json({ status: 2 });
+            }
+        })
+        .catch(err => res.json(err));
+});
 
-app.listen(3001, () =>
-{
-    console.log('server is running...')
-})
+app.put('/forgetps', (req, res) => {
+    const { email, password } = req.body;
+    usermodel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                usermodel.updateOne({ email: email }, { password: password })
+                    .then(() => res.json({ status: 1 }))
+                    .catch(err => res.json(err));
+            } else {
+                res.json({ status: 2 });
+            }
+        })
+        .catch(err => res.json(err));
+});
+
+app.listen(3001, () => {
+    console.log('server is running...');
+});
