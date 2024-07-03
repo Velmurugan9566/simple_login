b8bd274d7f7791ad622a31b1a8454f6549c5dbc4
_Vel_Murugan_ <140421065+Velmurugan9566@users.noreply.github.com>
2024-07-03 21:14:06 +0530

    Update Login.jsx
    add spinner button in login form

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -7,21 +7,23 @@ import './App.css'
 import axios from 'axios'
 import { Link, useNavigate } from 'react-router-dom';
  axios.defaults.withCredentials =true
-
+import Spinner from 'react-bootstrap/Spinner';
 
 function Login() {
   const [email, setemail] = useState('')
   const navigate = useNavigate()
   const [password, setps] = useState('')
-
+ const [load, setload] = useState(false)
   var status = 0
   function handle(e) {
     // alert('helllo')
     e.preventDefault()
     if (email.length != 0) {
       if (password.length != 0) {
+       setload(true)
         axios.post('https://simple-login-api.vercel.app/login', { email, password })
           .then(res => {
+           setload(false)
             status = res.data.status
             console.log(res)
             if (status == 1) {
@@ -39,6 +41,7 @@ function Login() {
           }
           )
           .catch(err => {
+           setload(false)
            toast.warning('some network error...')
           })
       }
@@ -70,9 +73,17 @@ function Login() {
               <input type='password' placeholder='Enter Password' autoComplete='off' name='password' className='form-control rounded-2' onChange={(e) => setps(e.target.value)} ></input>
             </div>
 
-            <button type='submit' className='btn btn-success w-100 rounded-2'>
-              Login
-            </button>
+            {
+              load ?
+                <button className='btn btn-success w-100 rounded-2'>
+                  <Spinner animation='border' variant='light'>
+                  </Spinner>
+                </button>
+                :
+                <button type='submit' className='btn btn-success w-100 rounded-2'>
+                  Login
+                </button>
+            }
             <Link to='/forgetps' ><p className='forgetps' >Forget Passward</p></Link>
             <p>Don't have an account</p>
             <Link to='/' className='btn btn-default border w-100 bg-light'>SignUp</Link>
