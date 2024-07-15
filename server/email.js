7000f1e93da4b0c2a6f2d6924a3e942aed8216b8
_Vel_Murugan_ <140421065+Velmurugan9566@users.noreply.github.com>
2024-07-15 22:25:05 +0530

    Update email.js
    email ps

--- server/email.js
+++ server/email.js
@@ -1,23 +1,23 @@
 var nodemailer = require('nodemailer');
 //var maill='muruganveltvl@gmail.com';
-async function mailsend(maill){
+function mailsend(maill){
   var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: 'fundamentaltk@gmail.com',
-      pass: ''
+      pass: 'ovwyvqxcminsvxzc'
     }
   });
   const msg='<h2>hey dude &#128513; </h2>'
   var mailOptions = {
     from: 'fundamentaltk@gmail.com',
     to: maill ,
-    subject: '<h2>Sending Email using Node.js <h2>',
+    subject: 'Login Status',
     html: msg
   };
   
   try{
-    await transporter.sendMail(mailOptions);
+    transporter.sendMail(mailOptions);
     console.log('Email send successfully')
   }catch(error){
     console.log('Error sending email: ',error)
